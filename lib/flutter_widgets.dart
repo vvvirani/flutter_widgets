35f7132b513158e9a0f99619e2dc446eb3109312
Vishal Virani <55572605+vvvirani@users.noreply.github.com>
2023-02-06 13:01:13 +0530

    add new widget

--- lib/flutter_widgets.dart
+++ lib/flutter_widgets.dart
@@ -6,6 +6,7 @@ export 'src/widgets/circle_icon_button.dart';
 export 'src/widgets/network_image_view.dart';
 export 'src/widgets/scrollable_column.dart';
 export 'src/widgets/state_ful_widget_wrapper.dart';
+export 'src/widgets/expanded_section.dart';
 
 export 'src/extensions/widget_ex.dart';
 
